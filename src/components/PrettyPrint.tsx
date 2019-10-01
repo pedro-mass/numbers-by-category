780af111177da3704f212582de97caa49f7e2fb9
Pedro Mass <pedro.j.mass@gmail.com>
2019-09-30 22:19:10 -0400

    PrettyPrint: added className
    Useful for adding targetted CSS

--- src/components/PrettyPrint.tsx
+++ src/components/PrettyPrint.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react'
 
 function PrettyPrint(props: Record<string, any>): JSX.Element {
-  return <pre>{JSON.stringify(props, null, 2)}</pre>
+  return <pre className="pretty-print">{JSON.stringify(props, null, 2)}</pre>
 }
 
 export default observer(PrettyPrint)
