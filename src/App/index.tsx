82d9e2ad81318898dde37ec181b897b61785b61d
Pedro Mass <pedro.j.mass@gmail.com>
2019-10-11 09:58:27 -0400

    add routing

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,21 +1,61 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+  LinkProps,
+  Redirect,
+  useRouteMatch,
+} from 'react-router-dom'
 import MobX from '../mobx'
 import './index.scss'
 
+const Link = (props: LinkProps) => {
+  const { to, children } = props
+  const path = typeof to === 'string' ? to : undefined
+  const match = useRouteMatch({ path })
+  console.log({ match })
+
+  const isActiveLink = path && match
+  const nonActiveLink = children ? (
+    <span className="link link-active">{children}</span>
+  ) : (
+    children
+  )
+
+  return (isActiveLink ? nonActiveLink : <RouterLink {...props} />) as any
+}
+
 const App: React.FC = () => {
   return (
-    <div className="app center">
-      <section className="header nav">
-        <span>context</span>
-        <span>mobx</span>
-        <span>mobx-state-tree</span>
-        <span>redux-starter-kit</span>
-      </section>
+    <Router>
+      <div className="app center">
+        <section className="header nav">
+          {/* todo: switch to normal ul/li for list of nav links */}
+          <Link to="/mobx">mobx</Link>
+          <span>mobx-state-tree</span>
+          <span>redux-starter-kit</span>
+          <span>context</span>
+        </section>
 
-      <section className="content">
-        <MobX />
-      </section>
-    </div>
+        <section className="content">
+          <Switch>
+            <Route exact path="/">
+              {/* todo: make a proper landing page */}
+              <Redirect
+                to={{
+                  pathname: '/mobx',
+                }}
+              />
+            </Route>
+            <Route path="/mobx">
+              <MobX />
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </Router>
   )
 }
 
