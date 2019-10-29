a7a0ea711fd6d4cb4109a28dbc256fc993fc58d8
Pedro Mass <pedro.j.mass@gmail.com>
2019-10-29 11:23:22 -0400

    scap everything in mobx-state-tree
    Start model off with TDD.
    Specify node version.

--- src/mobx-state-tree/components/PrettyPrint.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { observer } from 'mobx-react'
-
-function PrettyPrint(props: Record<string, any>): JSX.Element {
-  return <pre className="pretty-print">{JSON.stringify(props, null, 2)}</pre>
-}
-
-export default observer(PrettyPrint)
--- src/mobx-state-tree/index.tsx
+++ src/mobx-state-tree/index.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
-import { CategoryListModel, List, ListActions } from './components/Category'
-import { Total } from './components/Total'
 
 if (process.env.REACT_APP_USE_MOBX_LOGGER === 'true') {
   import('mobx-logger').then(logger => logger.enableLogging())
 }
 
 const Mobx: React.FC = () => {
-  const categories = new CategoryListModel()
-
-  const addCategory = (): void => categories.add()
-  const resetBalances = (): void => categories.resetTotals()
-  const deleteCategories = (): void => categories.reset()
-
   return (
-    <div className="mobx center">
-      <List categories={categories} />
-      <ListActions
-        addCategory={addCategory}
-        resetBalances={resetBalances}
-        deleteCategories={deleteCategories}
-      />
-      <Total className="summary" categories={categories} />
+    <div className="center">
+      <h1>Todo</h1>
     </div>
   )
 }
